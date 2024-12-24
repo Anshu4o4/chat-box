42ed9baa94778078aa8f4f3cbcf8897c33c6d1f1
Anshu4o4 <anshu4o4error@gmail.com>
2024-12-24 16:36:20 +0530

    6th

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,7 @@ from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+import { reportWebVitals } from 'web-vitals';
 
 load_dotenv()
 
